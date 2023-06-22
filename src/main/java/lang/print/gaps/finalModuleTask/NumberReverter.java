25543569ffcbaf49e93c934df0aa550ba1069df9
yuliyago <yuuliya02@proton.me>
2023-06-22 07:34:32 +0300

    Final task of the module3 was done..

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,9 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int first=(number%10)*100;
+        int second=(number%100)-(number%10);
+        int third=(number/100);
+        System.out.println(first+second+third);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,5 +2,6 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
+        System.out.println(temperatureCelsius*9/5.+32);
     }
 }
